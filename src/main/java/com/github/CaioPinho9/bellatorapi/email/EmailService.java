40480f8026a58225fcb6724924a7308c02eb8dcd
CaioPinho9 <69582667+CaioPinho9@users.noreply.github.com>
2022-07-22 14:08:19 -0300

    feat: gmail authentication

--- src/main/java/com/github/CaioPinho9/bellatorapi/email/EmailService.java
+++ src/main/java/com/github/CaioPinho9/bellatorapi/email/EmailService.java
@@ -8,8 +8,11 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.nio.charset.MalformedInputException;
+import java.util.Properties;
 
 @Service
 @AllArgsConstructor
@@ -22,14 +25,42 @@ public class EmailService implements EmailSender {
     @Override
     @Async
     public void send(String to, String email) {
+        String subject = "Confirm your email";
+        final String from = "mailbellator@gmail.com";
+        final String password ="yrlrlzxzuognsvzr";
+
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.debug", "true");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        Session session = Session.getDefaultInstance(props,
+        new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from,password);
+            }
+        });
+
         try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            Transport transport = session.getTransport();
+            InternetAddress addressFrom = new InternetAddress(from);
+
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setSender(addressFrom);
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setText(email, true);
             helper.setTo(to);
-            helper.setSubject("Confirm your email");
-            helper.setFrom("caiopinho9@gmail.com");
-            javaMailSender.send(mimeMessage);
+            helper.setSubject(subject);
+
+            transport.connect();
+            Transport.send(mimeMessage);
+            transport.close();
         } catch (MessagingException e) {
             LOGGER.error("failed to send email", e);
             throw new IllegalStateException("failed to send email");
